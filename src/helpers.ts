ba298671342c5bc9181f46b863bf91d5f393957f
Veaceslav <veaceslav.orlioglo@wiswm.com>
2025-03-14 18:04:52 +0200

    full copy of junit-xml-reporter

---
+++ src/helpers.ts
@@ -0,0 +1,43 @@
+import {
+    Duration,
+    PickleStep,
+    Step,
+    TestRunStarted,
+    TestStepResultStatus,
+    TimeConversion,
+} from '@cucumber/messages'
+import { DateTime } from 'luxon'
+
+export function durationToSeconds(duration?: Duration) {
+    if (!duration) {
+        return 0
+    }
+    return TimeConversion.durationToMilliseconds(duration) / 1000
+}
+
+export function countStatuses(
+    statuses: Record<TestStepResultStatus, number>,
+    predicate: (status: TestStepResultStatus) => boolean = () => true
+) {
+    return Object.entries(statuses)
+        .filter(([status]) => predicate(status as TestStepResultStatus))
+        .reduce((prev, [, curr]) => prev + curr, 0)
+}
+
+export function formatStep(step: Step, pickleStep: PickleStep, status: TestStepResultStatus) {
+    let text = `${step.keyword.trim()} ${pickleStep.text.trim()}.`
+    do {
+        text += '.'
+    } while (text.length < 76)
+    return text + status.toLowerCase()
+}
+
+export function formatTimestamp(testRunStarted: TestRunStarted | undefined) {
+    if (!testRunStarted) {
+        return undefined
+    }
+    const millis = TimeConversion.timestampToMillisecondsSinceEpoch(testRunStarted.timestamp)
+    return DateTime.fromMillis(millis, { zone: 'UTC' }).toISO({
+        suppressMilliseconds: true,
+    }) as string
+}
